22e56ec092f00ebf66f7f7bea4808275139d82e4
FernandoLepore <juliofernandolepore@gmail.com>
2022-08-04 21:49:57 -0300

    hola mundo con C

---
+++ hola mundo desde C.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+// importar la libreria estadar input y output
+/* ahora a desarrollar el algoritmo en la funcion principal
+o main */
+	
+	
+int main(){
+	//ahora a mandar a imprimir a consola, la "f" significa "format"
+	
+	printf("hola mundo desde c...");
+		return 0;
+}
+
