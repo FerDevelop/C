1969996ac5d121adfd5b914b1172a08ef235d3d3
FernandoLepore <juliofernandolepore@gmail.com>
2022-08-12 10:58:09 -0300

    Operadores Pre - Post Incremento, Pre - Post Decremento, en C

--- operadores de incremento y decremento en C.c
+++ operadores de incremento y decremento en C.c
@@ -14,7 +14,23 @@ int main() {
 	//PRE-INCREMENTO
 	++a;
 	printf("\nEl valor de a luego de ++a quedo en: %d", a);
+	//un ejemplo para observar las difrencias
+	a = 5;
+	b = 2;
+	c = ++a * b++;
+	printf("\nValor de a: %d", a);// no tenia pendiente incremento alguno.
+	printf("\nValor de b: %d", b);
+	// tenia pendiente un incremento y aumenta porque se uso en la funcion printf
+	printf("\nValor de c: %d", c);
+	// ++a (el valor se incrementa antes de usarse
+	// b++ (el valor se incrementa en la proxima iteracion)
 	
+	//OPERADORES DE DECREMENTO: PRE-DECREMENTO Y POST-DECREMENTO
+	a = 5;
+	b = 2;
+	c = --a * b--; // aca sigue valiendo 2 porque nu fue usada la variable
+	printf("\nValor de c: %d", c);
+	//EL MAS COMUN ES EL OPERADOR POST-INCREMENTO (a++) por ejemplo en los ciclos.
 	return 0;
 }
 
