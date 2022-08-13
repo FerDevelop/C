2d7428d58bd52242d9d78a4baf7d2999659b2909
Fernando Lepore <juliofernandolepore@gmail.com>
2022-08-13 12:30:19 -0300

    operadores logicos and, or, not

--- operadores logivos en C.c
+++ operadores logivos en C.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
-
+#include <stdbool.h>
 int main() {
-	//Recordar observar la TABLA DE VERDAD (y, o, not)
+	//Recordar observar la TABLA DE VERDAD
 	
 	//OPERADORES LOGICOS 
+	bool a = true; //verdadero
+	bool b = false; // falso
+	
+	printf("El valor de a es: %d", a);
+	printf("\nEl valor de b es: %d", b);
 	
 	return 0;
 }
