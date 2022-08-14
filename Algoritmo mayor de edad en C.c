56fad288cdd3cefba966e08af26f05aa2aba5e3c
FernandoLepore <juliofernandolepore@gmail.com>
2022-08-14 18:06:51 -0300

    algoritmo mayor de edad

--- Algoritmo mayor de edad en C.c
+++ Algoritmo mayor de edad en C.c
@@ -2,7 +2,17 @@
 
 int main() {
 	//Creando Algoritmo mayor de Edad
-	
+	const int edadAdulto = 18;
+	int edadProporcionada = 0;
+	printf("Proporciona tu edad\n");
+	scanf("%d", &edadProporcionada);
+	//ahora verificamos si es mayor de edad 
+	if (edadProporcionada >= edadAdulto){
+		printf("tu edad es: %d y sos mayor de edad", edadProporcionada);
+	}
+	else {
+		printf("eres menor de edad porque tu edad es: %d", edadProporcionada);
+	}
 	return 0;
 }
 
