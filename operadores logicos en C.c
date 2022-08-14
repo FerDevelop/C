96ee081863c0f09bb09cf9e0e7f329c00366228a
FernandoLepore <juliofernandolepore@gmail.com>
2022-08-14 15:27:35 -0300

    operador AND,OR Y NOT

--- operadores logicos en C.c
+++ operadores logicos en C.c
@@ -12,7 +12,18 @@ int main() {
 	//OPERADOR LOGICO && (AND - Y)
 	//regresa verdadero si ambos operadores son verdaderos
 	bool c = a && b;
-	printf("\nEl valor de c es: %d", c);
+	printf("\nEl valor de c en operador AND es: %d", c);
+		
+	//OPERADOR LOGICO ||(OR)
+	c = a || b;
+	//regresa verdadero si alguno de los 2 operandos sea verdadero o (1)
+	//solo si ambos son falsos entonces el valor es falso o (0)
+	printf("\nEl valor de c en el operador OR es: %d", c);
+	
+	//OPERADOR LOGICO NOT (OPERADOR NO)
+	//INVIERTE EL VALOR ORIGINAL(CONVIERTE EL VALOR DE 0 A 1 Y VICEVERSA)
+	c = !a; //a fue declarado en un inicio como TRUE entonces el resultado sera 0(false)
+	printf("\nEl valor de c en el operador NOT es: %d", c);
 	
 	return 0;
 }
