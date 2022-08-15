ad51a2f6d9b23be396bdfa10ec121fa4cec4ae2d
FernandoLepore <juliofernandolepore@gmail.com>
2022-08-14 21:35:41 -0300

    Ciclo For en C

---
+++ ciclo For en c.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main() {
+	//CICLO FOR - Ciclo Para
+	
+	for(int contador = 1; contador <= 5; contador++){
+		//aca tambien se puede usar sintaxis simplificada de una linea
+		printf("buenos dias\n");
+		printf("valor de contador: %d\n", contador);
+	}
+	
+	return 0;
+}
+
